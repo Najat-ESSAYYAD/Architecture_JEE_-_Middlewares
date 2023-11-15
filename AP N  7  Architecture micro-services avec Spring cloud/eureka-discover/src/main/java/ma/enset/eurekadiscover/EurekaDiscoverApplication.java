96d5963a2a9f56f103eaf8807701d9cffd6bf750
najat <essayyad.najat@gmail.com>
2023-11-16 00:18:35 +0100

    commit2

---
+++ AP N  7  Architecture micro-services avec Spring cloud/eureka-discover/src/main/java/ma/enset/eurekadiscover/EurekaDiscoverApplication.java
@@ -0,0 +1,14 @@
+package ma.enset.eurekadiscover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class EurekaDiscoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoverApplication.class, args);
+    }
+
+}
