ef4512547b9d874616d44bb79d5cae205e1d1a2d
najat <essayyad.najat@gmail.com>
2023-11-15 22:01:02 +0100

    commit

---
+++ AP N° 6  Mise en oeuvre d'un micro-service/bank-account-service/src/main/java/ma/enset/bankaccountservice/service/AccountService.java
@@ -0,0 +1,8 @@
+package ma.enset.bankaccountservice.service;
+
+import ma.enset.bankaccountservice.dto.BankAccountRequestDTO;
+import ma.enset.bankaccountservice.dto.BankAccountResponseDTO;
+
+public interface AccountService {
+     BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO);
+}
